7b81e105e287c8071f85bf33d07db7b4352758f6
Cid <cdubs8600@gmail.com>
2015-06-29 16:28:14 -0700

    new files

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,219 @@
-(function ($, videojs) {
-	$(document).ready(function() {
-	  $('.btn-fb a').click(function(e) {
-	    e.preventDefault();
-	    FB.ui({
-	    method: 'share',
-	    href: 'https://developers.facebook.com/docs/',
-	  }, function(response){});
+(function($) {
 
-	  });
+    // Helper Function for animations
+    function isScrolledIntoView($el, offset) {
 
-    videojs.options.flash.swf = "/js/videojs/video-js.swf";
+        offest = offset || 0;
+      var $window = $(window);
 
-    videojs('vid1', { "techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v=L0s63Jdbwk0" }).ready(function() {
+      var docViewTop = $window.scrollTop();
+      var docViewBottom = docViewTop + $window.height();
 
-      this.one('ended', function() {
-        this.src('http://www.youtube.com/watch?v=jofNR_WkoCE');
-      this.play();
-      });
+      var elemTop = $el.offset().top;
+      var elemBottom = elemTop + $el.height() + offset;
+
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    // animate elements in hero
+    $(function (cb) {
+
+        var $heroEl, $heroLock, $heroImg, classUp, classDown, $window;
+        var isIE9 = (navigator.userAgent.indexOf("MSIE 9") > -1) ? true : false;
+
+        $window = $(window);
+        $heroEl = $('#hero-module');
+        $heroLock = $('.left-col img', $heroEl);
+        $heroImg = $('.heroes', $heroEl);
+
+        classUp = 'animated fadeInUp';
+        classDown = 'animated fadeInDown';
+
+        function checkScroll($el, namespace, classes) {
+            if (cb($el, 0)) {
+                $el.css('visibility', 'visible').addClass(classes);
+                $window.off('scroll[' + namespace + ']');
+            }
+        }
+
+        if (!isIE9) {
+            $window.on('scroll.up', function (e) {
+                checkScroll($heroImg, 'up', classUp);
+            });
+
+            $window.on('scroll.down', function (e) {
+                checkScroll($heroLock, 'down', classDown);
+            });
+
+            $window.on('load', function (e) {
+                checkScroll($heroLock, 'down', classDown);
+                checkScroll($heroImg, 'up', classUp);
+            });
+        } else {
+            $heroLock.css('visibility', 'visible');
+            $heroImg.css('visibility', 'visible');
+        }
+
+
+    }(isScrolledIntoView));
+
+    // Counter App
+    $(function(cb) {
+        var isIE9 = (navigator.userAgent.indexOf("MSIE 9") > -1) ? true : false;
+
+        var clock, opts, $el, classLeft, $window;
+
+        opts = {
+            targetEls: [".counter"],
+            network: "sales.fyre.co",
+            siteId: "371327",
+            articleIds: ["CURATE"]
+        };
+        $window = $(window);
+        classLeft = 'animated fadeInLeft';
+        $el = $('#cars');
+
+        if (!isIE9) {
+            clock = window.clock = new FlipCounter(opts);
+
+            $window.on('scroll.cars', function (e) {
+                if (cb($el.parent(), 200)) {
+                    $el.css('visibility', 'visible').addClass(classLeft);
+                    $window.off('scroll.cars');
+                }
+            });
+
+            $window.on('load', function (e) {
+                if (cb($el.parent(), 200)) {
+                    $el.css('visibility', 'visible').addClass(classLeft);
+                    $window.off('scroll.cars');
+                }
+            });
+
+        } else {
+
+            $el.css('visibility', 'visible');
+            var counter = new LfCommentCounts(opts, createClock);
+
+        }
+
+        function createClock(data) {
+
+            var contentCount = data.data[opts.siteId][opts.articleIds[0]].total; // presumes only the first articleId is important
+            var string = '<ul class="flip nocomma"><li class="flip-clock-before"><a href="#"><div class="up"><div class="shadow"></div><div class="inn">{{n}}</div></div><div class="down"><div class="shadow"></div><div class="inn">{{n}}</div></div></a></li><li class="flip-clock-active"><a href="#"><div class="up"><div class="shadow"></div><div class="inn">{{n}}</div></div><div class="down"><div class="shadow"></div><div class="inn">{{n}}</div></div></a></li></ul>';
+            contentCount = (""+contentCount).split("");
+            var markup = "";
+
+            for (var i = 0; contentCount.length > 0; ++i) {
+              var digit = string.replace(/{{n}}/g, contentCount.pop());
+              if ( i % 2 === 0 && i > 0) {
+                digit = digit.replace(/nocomma/g, 'comma');
+                i = -1;
+              }
+              markup = digit + markup;
+            }
+
+            $(opts.targetEls[0]).addClass('flip-clock-wrapper').append(markup);
+
+        }
+
+    }(isScrolledIntoView));
+
+    // More Mediawall App
+    $(function() {
+
+        Livefyre.require(['streamhub-wall#3.3.2'], function(MediaWall) {
+            var wall = window.wall = new MediaWall({
+                el: document.getElementById("wall"),
+                collection: {
+                    "network": "indycar.fyre.co",
+                    "siteId": "369833",
+                    "articleId": "custom-1434402399842"
+                },
+                initial: 9,
+                showMore: 12
+            });
+
+            wall._collection.on('data', function() {
+                clock._clockInstances[0].increment();
+            });
+
+            wall._wallView.$el.on('removeContentView.hub', function(e) {
+                clock._clockInstances[0].decrement();
+            });
+
+        });
+
+        $('#wallShowMore').click(function(e) {
+            e.preventDefault();
+            wall._wallView.showMore();
+        });
+
     });
-	});
-}(jQuery, videojs));
+
+    // Top Mediawall App
+    $(function() {
+        Livefyre.require(['streamhub-wall#3.3.2'], function(MediaWall) {
+            var walltop = window.walltop = new MediaWall({
+                el: document.getElementById("top-wall"),
+                collection: {
+                    "network": "indycar.fyre.co",
+                    "siteId": "369833",
+                    "articleId": "custom-1434401721002"
+                },
+                initial: 3
+            }).pause();
+        });
+    }());
+
+    // Poll App
+    $(function() {
+        Livefyre.require(["poll#0"], function(a) {
+            (new a({
+                networkId: "indycar.fyre.co",
+                siteId: "369833",
+                pollId: "e6cd815213af11e5b62e12b19652bf83",
+                env: "prod"
+            })).render(document.getElementById("lf-poll-e6cd815213af11e5b62e12b19652bf83"));
+        });
+    }());
+
+    // Trending App
+    $(function() {
+        Livefyre.require(['//cdn.livefyre.com/libs/app-embed/v0.6.4/app-embed.min.js'], function(appEmbed) {
+            appEmbed.loadAll().done(function(embed) {
+                embed = embed[0];
+                embed.el.onload(embed.getConfig());
+            });
+        });
+    }());
+
+    // Video Player
+    $(function() {
+        videojs.options.flash.swf = "/js/videojs/video-js.swf";
+
+        videojs('vid1', {
+            "techOrder": ["youtube"],
+            "src": "http://www.youtube.com/watch?v=L0s63Jdbwk0"
+        }).ready(function() {
+
+            this.one('ended', function() {
+                this.src('http://www.youtube.com/watch?v=jofNR_WkoCE');
+                this.play();
+            });
+        });
+    }());
+
+    //  FB Share
+    $(function() {
+        $('.btn-fb a').click(function(e) {
+            e.preventDefault();
+            FB.ui({
+                method: 'share',
+                href: 'https://developers.facebook.com/docs/',
+            }, function(response) {});
+
+        });
+    }());
+
+}(jQuery));
