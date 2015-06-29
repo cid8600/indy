ed9bd658875b5485cf255256b9be2a55839dcaec
Sonyl Nagale <sonyl@nagale.com>
2015-06-29 15:24:33 -0400

    Branching for fit & finish tasks

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,31 @@
         };
 
         clock = window.clock = new FlipCounter(opts);
-
+        
+        $container = $('#social-counter-moudle');
+        addClass();
+        $(window).scroll(addClass);
+        
+        function addClass() {
+          if (isScrolledIntoView($container)) {
+            $('#social-counter-moudle').addClass('animated fadeInLeft');
+            $(window).off('scroll');
+          }
+        }
+        
+        function isScrolledIntoView(elem) {
+          var $elem = $(elem);
+          var $window = $(window);
+  
+          var docViewTop = $window.scrollTop();
+          var docViewBottom = docViewTop + $window.height();
+  
+          var elemTop = $elem.offset().top;
+          var elemBottom = elemTop + $elem.height();
+  
+          return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        }
+        
     }());
 
     // More Mediawall App
